562639ad9384e886af2b9288a679e608c81a0fe2
Юлия Бровина <Tyuiu.BrovinaYV@outlook.com>
2023-10-08 00:11:23 +0500

    Task1

---
+++ Tyuiu.BrovinaYV.Sprint2.Task1.V17.Lib/DataService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.BrovinaYV.Sprint2.Task1.V17.Lib
+{// (==, !=, <, >, <=, >=, последовательность можно чередовать, но использовать один раз в выражении) и логических операций (|, &, ||, &&, !, ^, последовательность операций не должна нарушаться),
+ // а также арифметических выражений, которая вернет логическую последовательность(массив): (True, False, True, True, True, False), при a = 135, b = 123, c = 455, d = 321
+    public class DataService : ISprint2Task1V17
+    {
+        public bool[] GetLogicOperations(int a, int b, int c, int d)
+        {
+            bool[] res = new bool[6];
+            res[0] =(a>b) | (c<d) ; //t
+            res[1] = (a<b) & (c>d);//f
+            res[2] = (a > b) || (c < d);//t
+            res[3] = (a < b+13) && (c != d); //t
+            res[4] = !(res[1]) ; //t
+            res[5] = (a>b)^(c>=d) ; //f
+            
+            return res;
+
+        }
+    }
+}
---
+++ Tyuiu.BrovinaYV.Sprint2.Task1.V17/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.BrovinaYV.Sprint2.Task1.V17.Lib;
+
+namespace Tyuiu.BrovinaYV.Sprint2.Task1.V17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            int a = 135;
+            int b = 123;
+            int c = 455;
+            int d = 321;
+            bool[] res = new bool[6];
+            res = ds.GetLogicOperations(a, b, c, d);
+            Console.Title = "Спринт #2 | Выполнила: Бровина Ю. В. | АСОиУБ-23-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Алгоритмы разветляющей структуры                                  *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #17                                                             *");
+            Console.WriteLine("* Выполнила Бровина Юлия Викторовна | АСОиУБ-23-2                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу из операций сравнений (==, !=, <, >, <=, >=)         *");
+            Console.WriteLine("* и логических операций (|, &, ||, &&, !, ^)                              *");
+            Console.WriteLine("* последовательность(массив):(True, False, True, True, True, False)       *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("a = " + a);
+            Console.WriteLine("b = " + b);
+            Console.WriteLine("c = " + c);
+            Console.WriteLine("d = " + d);
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            for (int i=0; i<6;i++)
+            {
+                Console.WriteLine(res[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
